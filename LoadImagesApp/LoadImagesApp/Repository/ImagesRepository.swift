0e75ec9a6dc8bbf3083c466fa248971795a3dc4d
Alex <sasha.1.sas@mail.ru>
2021-12-16 03:59:23 +0700

    Закончил работу над нетворк сервисом

---
+++ LoadImagesApp/LoadImagesApp/Repository/ImagesRepository.swift
@@ -0,0 +1,30 @@
+//
+//  ImagesRepository.swift
+//  LoadImagesApp
+//
+//  Created by Александр Фомин on 14.12.2021.
+//
+
+import Foundation
+import UIKit
+
+final class ImagesRepository
+{
+	static let shared = ImagesRepository()
+	private var images: [UIImage]
+	
+	private init() {
+		self.images = [UIImage]()
+	}
+}
+
+extension ImagesRepository
+{
+	func append(_ image: UIImage) {
+		self.images.append(image)
+	}
+	
+	func getImages() -> [UIImage] {
+		self.images
+	}
+}
